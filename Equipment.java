dfff6360d27ae4f3b3132974d8f45e73aa357f9c
JitseVDB <jitse.vandenberghe@outlook.be>
2025-04-27 15:04:44 +0200

    Added isValidIdentification to Equipment.

--- Equipment.java
+++ Equipment.java
@@ -1,3 +1,8 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import be.kuleuven.cs.som.annotate.*;
 
 /**
@@ -6,8 +11,14 @@ import be.kuleuven.cs.som.annotate.*;
  * Each piece of equipment has a weight, a base value, an identification number,
  * and can be owned by an entity or be stored in a backpack.
  *
- * @invar Each piece of equipment must have a valid weight.
- *      | isValidWeight(getWeight())
+ * @invar   Each piece of equipment must have a valid weight.
+ *          | isValidWeight(getWeight())
+ *
+ * @invar   Each piece of equipment must have a valid identification number.
+ *          | isValidIdentification(getClass(), getIdentification())
+ *
+ * @invar   Each piece of equipment must have a valid base value.
+ *          | isValidValue(getValue)
  *
  * @author Jitse Vandenberghe
  *
@@ -31,26 +42,75 @@ public abstract class Equipment {
      *
      * @post    The given weight is registered as the weight of this equipment.
      *          | new.getWeight() == weight
+     *
      * @post    The given identification number is registered as the identification number.
      *          | new.getIdentification() == identification
+     *
      * @post    The given base value is registered as the base value.
      *          | new.getBaseValue() == baseValue
      *
+     * @effect  The identification number is added to the map to keep track of all identification numbers
+     *          for each equipment type.
+     *          | addIdentification(this.getClass(), identification)
+     *
      * @throws  IllegalArgumentException
-     *          If the given weight is negative or the base value is not valid.
-     *          | weight < 0 || !isValidValue(baseValue)
+     *          If the given weight is invalid.
+     *          |!isValidWeight(weight)
+     *
+     * @throws  IllegalArgumentException
+     *          If the given identification number is invalid.
+     *          | !isValidIdentification(this.getClass(), identification)
+     *
+     * @throws  IllegalArgumentException
+     *          If the given base Value is invalid.
+     *          | !isValidValue(baseValue)
      */
     public Equipment(int weight, long identification, int baseValue)
             throws IllegalArgumentException {
 
         if (!isValidWeight(weight))
             throw new IllegalArgumentException("Weight cannot be negative.");
+        if (!isValidIdentification(this.getClass(), identification))
+            throw new IllegalArgumentException("Identification cannot be negative and must be unique.");
         if (!isValidValue(baseValue))
             throw new IllegalArgumentException("Base value must be between 1 and 1000.");
 
         this.weight = weight;
         this.identification = identification;
         this.baseValue = baseValue;
+
+        // Adds identification number to map to keep track of all identification numbers for each equipment type.
+        addIdentification(this.getClass(), identification);
+    }
+
+    /**********************************************************
+     * Weight
+     **********************************************************/
+
+    /**
+     * The weight of this piece of equipment.
+     */
+    private final int weight;
+
+    /**
+     * Returns the weight of this piece of equipment.
+     */
+    @Raw @Basic
+    public int getWeight() {
+        return weight;
+    }
+
+    /**
+     * Check whether the given weight is a valid weight for a piece of equipment.
+     *
+     * @param   weight
+     *          The weight to check.
+     *
+     * @return  True if and only if the given weight is positive.
+     *          | result == (weight >= 0)
+     */
+    public static boolean isValidWeight(int weight) {
+        return (weight >= 0);
     }
 
     /**********************************************************
@@ -63,6 +123,11 @@ public abstract class Equipment {
     private final long identification;
 
     /**
+     * Static set containing all the identification numbers
+     */
+    private static Map<Class<?>, Set<Long>> equipmentByType = new HashMap<>();
+
+    /**
      * Returns the identification number of this piece of equipment.
      */
     @Raw @Basic
@@ -70,60 +135,68 @@ public abstract class Equipment {
         return identification;
     }
 
-    /**********************************************************
-     * Backpack
-     **********************************************************/
+    public static void addIdentification(Class<?> equipmentType, long identification) {
+        // Get the existing set of ID's for the given type of equipment
+        Set<Long> existingIDs = equipmentByType.get(equipmentType);
 
-    /**
-     * Variable referencing the backpack (if any) in which this equipment is stored.
-     */
-    private Backpack backpack = null;
+        // If the set does not exist, create a new set associated with the equipmentType in the equipmentByType map
+        if (existingIDs == null) {
+            existingIDs = new HashSet<>();
+            equipmentByType.put(equipmentType, existingIDs);
+        }
 
-    /**
-     * Returns the backpack in which this equipment is stored.
-     */
-    @Raw @Basic
-    public Backpack getBackpack() {
-        return backpack;
+        // Add the identification number to the set associated with the equipmentType
+        existingIDs.add(identification);
+
     }
 
     /**
-     * Set the backpack in which this equipment is stored.
+     * Check whether the given identification number is a valid identification number for this piece of equipment.
      *
-     * @param   backpack
-     *          The new backpack to set, or null if the equipment is not stored.
-     */
-    @Raw @Basic
-    public void setBackpack(Backpack backpack) {
-        this.backpack = backpack;
-    }
-
-    /**********************************************************
-     * Owner
-     **********************************************************/
-
-    /**
-     * Variable referencing the entity (if any) that owns this piece of equipment.
-     */
-    private Entity owner = null;
-
-    /**
-     * Returns the entity that currently owns this piece of equipment.
-     */
-    @Raw @Basic
-    public Entity getOwner() {
-        return owner;
-    }
-
-    /**
-     * Sets the owner of this piece of equipment.
+     * @param   equipmentType
+     *          The class type of the equipment for which the identification number is being validated.
      *
-     * @param   owner
-     *          The entity that will own this equipment, or null if the item doesn't have an owner.
+     * @param   identification
+     *          The identification number to check.
+     *
+     * @return  True if the identification number is positive and unique within the given equipment type;
+     *          false otherwise.
+     *          | result == (identification >= 0) && (isUniqueForType(instance of this ,getIdentification())
      */
-    @Raw @Basic
-    public void setOwner(Entity owner) {
-        this.owner = owner;
+    public boolean isValidIdentification(Class<?> equipmentType, long identification) {
+        return (identification >= 0) && (isUniqueForType(equipmentType, identification));
+    }
+
+    /**
+     * Check wether the given identification number is unique for the specified equipment type.
+     *
+     * @param   equipmentType
+     *          The class type of the equipment for which the identification number is being validated.
+     *
+     * @param   identification
+     *          The identification number to check for uniqueness.
+     *
+     * @return  True if the identification number is unique for the given equipment type;
+     *          false if the identification number already exists for that equipment type.
+     *          | result == (existingIDs == null || !existingIDs.contains(identification))
+     */
+    public static boolean isUniqueForType(Class<?> equipmentType, long identification) {
+        // Get the existing set of ID's for the given type of equipment
+        Set<Long> existingIDs = equipmentByType.get(equipmentType);
+
+        // Iterate over every ID in the set for this equipment type
+        if (existingIDs != null) {
+            for (Long existingID : existingIDs) {
+                // If the ID already exists, return false
+                if (existingID == identification) {
+                    return false;  // ID is not unique
+                }
+            }
+        }
+
+        // If no matching ID is found, return true (ID is unique)
+        return true;
+
     }
 
     /**********************************************************
@@ -174,32 +247,59 @@ public abstract class Equipment {
     protected abstract int calculateCurrentValue();
 
     /**********************************************************
-     * Weight
+     * Backpack -> verder uitwerken later
      **********************************************************/
 
     /**
-     * The weight of this piece of equipment.
+     * Variable referencing the backpack (if any) in which this equipment is stored.
      */
-    private final int weight;
+    private Backpack backpack = null;
 
     /**
-     * Returns the weight of this piece of equipment.
+     * Returns the backpack in which this equipment is stored.
      */
     @Raw @Basic
-    public int getWeight() {
-        return weight;
+    public Backpack getBackpack() {
+        return backpack;
     }
 
     /**
-     * Check whether the given weight is a valid weight for a piece of equipment.
+     * Set the backpack in which this equipment is stored.
      *
-     * @param   weight
-     *          The weight to check.
-     *
-     * @return  True if and only if the given weight is not negative.
-     *          | result == (weight >= 0)
+     * @param   backpack
+     *          The new backpack to set, or null if the equipment is not stored.
      */
-    public static boolean isValidWeight(int weight) {
-        return (weight >= 0);
+    @Raw @Basic
+    public void setBackpack(Backpack backpack) {
+        this.backpack = backpack;
     }
+
+    /**********************************************************
+     * Owner -> verder uitwerken later
+     **********************************************************/
+
+    /**
+     * Variable referencing the entity (if any) that owns this piece of equipment.
+     */
+    private Entity owner = null;
+
+    /**
+     * Returns the entity that currently owns this piece of equipment.
+     */
+    @Raw @Basic
+    public Entity getOwner() {
+        return owner;
+    }
+
+    /**
+     * Sets the owner of this piece of equipment.
+     *
+     * @param   owner
+     *          The entity that will own this equipment, or null if the item doesn't have an owner.
+     */
+    @Raw @Basic
+    public void setOwner(Entity owner) {
+        this.owner = owner;
+    }
+
 }
